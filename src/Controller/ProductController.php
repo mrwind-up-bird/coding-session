55b9419975498f94b127778cabef3696efcafd34
Oliver Baer <oliver.baer@hansefit.de>
2023-09-05 02:34:06 +0200

    first commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductController extends AbstractController
+{
+    private const CSV_FIELD_SEPERATOR = ';';
+    private const CSV_FIELD_ENCLOSURE = '"';
+
+    public function __construct(private ProductRepository $productRepository, private SerializerInterface $serializer, private EntityManagerInterface $entityManager) {
+
+    }
+
+    #[Route('/', name: 'app_root')]
+    public function index(): Response
+    {
+        $products = $this->productRepository->findAll();
+
+        return $this->render('products/products.html.twig', ['products' => $products]);
+    }
+
+    #[Route('/upload', name: 'app_product_upload')]
+    public function upload(Request $request): Response
+    {
+        $form = $this->createForm(FileType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->getData();
+
+            if ($uploadedFile && $this->isCsvFile($uploadedFile)) {
+                $this->createOrUpdateProducts($uploadedFile);
+                $this->addFlash('success', 'File successfully uploaded...');
+            }
+            else {
+                $this->addFlash('error', 'Error uploading File');
+            }
+            $this->redirectToRoute('app_product_upload');
+        }
+
+        return $this->render('upload/upload_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function createOrUpdateProducts($file)
+    {
+        $context = [
+            'csv_delimiter' => self::CSV_FIELD_SEPERATOR,
+            'csv_enclosure' => self::CSV_FIELD_ENCLOSURE,
+        ];
+
+        $content = $this->removeBomFromUtf8String(file_get_contents($file));
+        $data = $this->serializer->decode($content, 'csv', $context);
+
+        foreach ($data AS $row) {
+            $product = new Product();
+            $product->setProductId($row["Produktnummer"]);
+            $product->setProductName($row["Produktname"]);
+            $product->setProductPrice(floatval($row["Preis"]));
+            $product->setProductTaxValue(intval($row["Mwst_Prozent"]));
+            $product->setProductDescription($row["Beschreibung"]);
+
+            $this->entityManager->persist($product);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    private function isCsvFile($file) : bool
+    {
+        return $file->getClientOriginalExtension() === 'csv';
+    }
+
+    /**
+     * removes BOM Chars from UTF8 String.
+     */
+    private function removeBomFromUtf8String(string $string): string
+    {
+        if (substr($string, 0, 3) == chr(hexdec('EF')).chr(hexdec('BB')).chr(hexdec('BF'))) {
+            return substr($string, 3);
+        } else {
+            return $string;
+        }
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $productId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $productName = null;
+
+    #[ORM\Column]
+    private ?float $productPrice = null;
+
+    #[ORM\Column]
+    private ?int $productTaxValue = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $productDescription = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?string
+    {
+        return $this->productId;
+    }
+
+    public function setProductId(string $productId): static
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): static
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getProductPrice(): ?float
+    {
+        return $this->productPrice;
+    }
+
+    public function setProductPrice(float $productPrice): static
+    {
+        $this->productPrice = $productPrice;
+
+        return $this;
+    }
+
+    public function getProductTaxValue(): ?int
+    {
+        return $this->productTaxValue;
+    }
+
+    public function setProductTaxValue(int $productTaxValue): static
+    {
+        $this->productTaxValue = $productTaxValue;
+
+        return $this;
+    }
+
+    public function getProductDescription(): ?string
+    {
+        return $this->productDescription;
+    }
+
+    public function setProductDescription(?string $productDescription): static
+    {
+        $this->productDescription = $productDescription;
+
+        return $this;
+    }
+}
